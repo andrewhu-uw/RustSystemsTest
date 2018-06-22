0bf8973af69698c1ee8b4ab18cd370722da655ef
andrewhu-uw <andrewhu@uw.edu>
2018-06-22 16:50:30 -0700

    +explanation

--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 fn main() {
 let copy:String;
-let v: Vec<String>;
+let v;
 let sp:String;
 v = {
     let s = "hello world".to_string(); // String type implements Clone
@@ -9,6 +9,9 @@ v = {
     let vtmp : Vec<&str>= s.split_whitespace().collect::<Vec<&str>>();
     let mut vq = Vec::new();
     for token in vtmp {
+        // Cannot do token.clone(), because even though it creates separate data,
+        // that data will be dropped at the end of the scope block because it was not
+        // _moved_ out of this scope block
         vq.push(token.to_owned());
     }
     vq
